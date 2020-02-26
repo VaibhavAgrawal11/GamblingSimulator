0ad4d4426440bea6ab9df33151e38774981e5f11
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-26 17:17:44 +0530

    MODIFIED: Added feature to gamble once

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -x
-printf "Start bet with stake 100 	\n"
+printf "Gamble once\n"
 #CONSTANT
 BET=1
+WIN=1
+LOSE=0
 #VARIABLES
 stake=100
-
+#GAMBLE
+gamble=$((RANDOM%2))
+if(($gamble==$WIN))
+then
+	printf "won"
+	stake=$((stake+BET))
+else
+	printf "lost"
+	stake=$((stake-BET))
+fi
