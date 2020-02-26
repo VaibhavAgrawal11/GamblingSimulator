8ffb8f8688b8e366c20187214da7f5d450b81bf1
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-02-26 17:54:41 +0530

    MODIFIED: Added feature to gamble 20 days

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,23 +1,31 @@
 #!/bin/bash -x
-printf "Gamble till half stake remains or increases\n"
+printf "Gamble till 20 days\n"
 #CONSTANT
-BET=1
+BET=10
 WIN=1
 LOSE=0
 #VARIABLES
-stake=100
-stop=$((stake/2))
-goal=$((stake+stop))
+won=0
+lost=0
+days=20
 #GAMBLE
-while (( $stake>$stop && $stake<$goal ))
+while(( $days>0))
 do
-	gamble=$((RANDOM%2))
-	if(($gamble==$WIN))
-	then
-		printf "won"
-		stake=$((stake+BET))
-	else
-		printf "lost"
-		stake=$((stake-BET))
-	fi
+	stake=100
+	stop=$((stake/2))
+	goal=$((stake+stop))
+	while (( $stake>$stop && $stake<$goal))
+	do
+		gamble=$((RANDOM%2))
+		if(($gamble==$WIN))
+		then
+			won=$((won+1))
+			stake=$((stake+BET))
+		else
+			lost=$((lost+1))
+			stake=$((stake-BET))
+		fi
+	done
+days=$((days-1))
 done
+
